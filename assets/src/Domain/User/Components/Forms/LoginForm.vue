fd7ac6d223b224a0d4e41ebce5c28dceb4f2fc35
roman <ignashov-roman@mail.ru>
2020-10-19 19:44:31 +0300

    LoginForm adaptive
    Signed-off-by: roman <ignashov-roman@mail.ru>

---
+++ assets/src/Domain/User/Components/Forms/LoginForm.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-card class="elevation-10">
+    <v-form ref="loginForm">
+      <v-row justify="center" align="center" style="flex-direction: column">
+        <v-col cols="10" sm="8" md="10">
+          <v-sheet class="text-center">Введите данные для автоизации</v-sheet>
+          <v-alert v-if="notFound" type="error" transition="fade-transition">Пользователь не найден</v-alert>
+        </v-col>
+        <v-col cols="10" sm="8" md="8" class="pt-0 mt-0">
+          <control-email v-model="data.email" :error="getError.email"/>
+        </v-col>
+        <v-col cols="10" sm="8" md="8">
+          <control-password v-model="data.password" :error="getError.password"/>
+        </v-col>
+        <v-col cols="10" sm="8" md="8" class="pt-0 mt-0 pb-0">
+          <v-layout justify-center>
+            <v-checkbox
+              class="pt-0 mt-0 align-self-center"
+              hide-details
+              v-model="data.rememberMe"
+              :error="getError.rememberMe"
+              label="Запомнить меня"
+            />
+          </v-layout>
+        </v-col>
+      </v-row>
+
+      <v-divider></v-divider>
+      <v-card-actions class="justify-center pb-2 pt-2">
+        <v-btn
+          class="pa2"
+          color="primary"
+          @click="submit"
+          :loading="loading"
+          x-large
+          elevation="24"
+        >Войти</v-btn>
+      </v-card-actions>
+    </v-form>
+  </v-card>
+</template>
+<script lang="ts">
+import {Component, Prop, Vue} from 'vue-property-decorator';
+import ControlEmail from "../../../App/Components/FormElements/ControlEmail.vue";
+import ControlPassword from "../../../App/Components/FormElements/ControlPassword.vue";
+import LoginRequest from "../../Entity/API/Login/LoginRequest";
+import {UserModule} from "../../UserModule";
+
+@Component({components: { ControlEmail, ControlPassword}})
+export default class LoginForm extends Vue {
+    @Prop() error: LoginRequest;
+    @Prop() notFound: boolean;
+
+    private data: LoginRequest =  {
+        email: '',
+        password: '',
+        rememberMe: false
+    };
+
+    get getError(): LoginRequest {
+      return this.error || { email: '', password: '', rememberMe: false }
+    }
+    get loading() { return UserModule.isLoading }
+
+    public submit() {
+        if(this.$refs.loginForm.validate()) {
+            this.$emit('submit', this.data);
+        }
+    }
+}
+</script>
---
+++ assets/src/Domain/User/Components/Forms/RegisterForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-card class="elevation-10">
+    <v-form ref="registerForm">
+      <v-row justify="center" align="center" style="flex-direction: column">
+        <v-col cols="10" sm="8" md="10" class="text-center">
+          <v-sheet>Введите данные для регистрации</v-sheet>
+        </v-col>
+        <v-col cols="10" sm="8" md="8"  class="pt-0 mt-0">
+          <control-email v-model="data.email" :error="getErrors.email"/>
+        </v-col>
+        <v-col cols="10" sm="8" md="8" class="pt-0 mt-0">
+          <control-password v-model="data.password" :error="getErrors.password"/>
+        </v-col>
+        <v-col cols="10" sm="8" md="8"  class="pa1">
+          <control-confirm
+            v-model="data.plainPassword"
+            :error="getErrors.plainPassword"
+            :field="data.password"
+            label="Подтверждение пароля"
+          />
+        </v-col>
+      </v-row>
+      <v-divider/>
+      <v-card-actions class="justify-center pb-2 pt-2">
+        <v-btn
+          class="pa2 text-center primary"
+          @click="register"
+          :loading="loading"
+          x-large
+          elevation="24"
+        >Зарегистрироваться</v-btn>
+      </v-card-actions>
+    </v-form>
+  </v-card>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from "vue-property-decorator";
+import ControlEmail from "../../../App/Components/FormElements/ControlEmail.vue";
+import ControlPassword from "../../../App/Components/FormElements/ControlPassword.vue";
+import ControlConfirm from "../../../App/Components/FormElements/ControlConfirm.vue";
+import {UserModule} from "../../UserModule";
+import RegisterByEmailRequest from "../../Entity/API/Register/ByEmail/RegisterByEmailRequest";
+
+@Component({components: { ControlEmail, ControlPassword, ControlConfirm}})
+export default class RegisterForm extends Vue  {
+    @Prop() errors: RegisterByEmailRequest;
+
+    private data: RegisterByEmailRequest = { email: '', password: '', plainPassword: ''};
+
+    get loading() { return UserModule.isLoading }
+    get getErrors(): RegisterByEmailRequest {
+        return this.errors || { email: '', password: '', plainPassword: '' }
+    }
+    register() {
+        if(this.$refs.registerForm.validate()) {
+            this.$emit('register', this.data);
+        }
+    }
+}
+</script>
